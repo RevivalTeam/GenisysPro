c9de0c89c611a747c4d430a564815eb30620045c
jj19971130 <1901014432@qq.com>
2018-02-13 19:51:59 +0800

    genisys

--- src/pocketmine/command/defaults/MakeServerCommand.php
+++ src/pocketmine/command/defaults/MakeServerCommand.php
@@ -44,79 +44,57 @@ class MakeServerCommand extends VanillaCommand {
 		$this->setPermission("pocketmine.command.makeserver");
 	}
 
-    /**
-     * @param CommandSender $sender
-     * @param string        $commandLabel
-     * @param array         $args
-     *
-     * @return bool
-     */
-    public function execute(CommandSender $sender, $commandLabel, array $args){
-        if(!$this->testPermission($sender)){
-            return false;
-        }
+	/**
+	 * @param CommandSender $sender
+	 * @param string        $commandLabel
+	 * @param array         $args
+	 *
+	 * @return bool
+	 */
+	public function execute(CommandSender $sender, string $commandLabel, array $args){
+		if(!$this->testPermission($sender)){
+			return false;
+		}
 
-        $server = $sender->getServer();
-        $pharPath = Server::getInstance()->getPluginPath() . DIRECTORY_SEPARATOR . "GenisysPro" . DIRECTORY_SEPARATOR . $server->getName() . "_" . $server->getPocketMineVersion() . "_" . date("Y-m-d") . ".phar";
-        if(file_exists($pharPath)){
-            $sender->sendMessage("Phar file already exists, overwriting...");
-            @unlink($pharPath);
-        }
+		$server = $sender->getServer();
+		$pharPath = Server::getInstance()->getPluginPath() . DIRECTORY_SEPARATOR . "Turanic" . DIRECTORY_SEPARATOR . $server->getName() . "_" . $server->getPocketMineVersion() . "_" . date("Y-m-d") . ".phar";
+		if(file_exists($pharPath)){
+			$sender->sendMessage("Phar file already exists, overwriting...");
+			@unlink($pharPath);
+		}
+        $sender->sendMessage($server->getName() . " Phar has begun to be created. This will take 4-5 minutes (may vary depending on your system).");
         $phar = new \Phar($pharPath);
-        $phar->setMetadata([
-            "name" => $server->getName(),
-            "version" => $server->getPocketMineVersion(),
-            "api" => $server->getApiVersion(),
-            "minecraft" => $server->getVersion(),
-            "protocol" => ProtocolInfo::CURRENT_PROTOCOL,
-            "creationDate" => time()
-        ]);
-        $phar->setStub('<?php define("pocketmine\\\\PATH", "phar://". __FILE__ ."/"); require_once("phar://". __FILE__ ."/src/pocketmine/PocketMine.php");  __HALT_COMPILER();');
-        $phar->setSignatureAlgorithm(\Phar::SHA1);
-        $phar->startBuffering();
+		$phar->setMetadata([
+			"name" => $server->getName(),
+			"version" => $server->getPocketMineVersion(),
+			"api" => $server->getApiVersion(),
+			"minecraft" => $server->getVersion(),
+			"protocol" => ProtocolInfo::CURRENT_PROTOCOL,
+			"creationDate" => time()
+		]);
+		$phar->setStub('<?php define("pocketmine\\\\PATH", "phar://". __FILE__ ."/"); require_once("phar://". __FILE__ ."/src/pocketmine/PocketMine.php");  __HALT_COMPILER();');
+		$phar->setSignatureAlgorithm(\Phar::SHA1);
+		$phar->startBuffering();
 
-        $filePath = substr(\pocketmine\PATH, 0, 7) === "phar://" ? \pocketmine\PATH : realpath(\pocketmine\PATH) . "/";
-        $filePath = rtrim(str_replace("\\", "/", $filePath), "/") . "/";
-        if(is_dir($filePath . ".git")){
-            // Add some Git files as they are required in getting GIT_COMMIT
-            foreach(new \RecursiveIteratorIterator(new \RecursiveDirectoryIterator($filePath . ".git")) as $file){
-                $path = ltrim(str_replace(["\\", $filePath], ["/", ""], $file), "/");
-                if((strpos($path, ".git/HEAD") === false and strpos($path, ".git/refs/heads") === false) or strpos($path, "/.") !== false){
-                    continue;
-                }
-                $phar->addFile($file, $path);
-                $sender->sendMessage("[GenisysPro] Adding $path");
-            }
-        }
-        foreach(new \RecursiveIteratorIterator(new \RecursiveDirectoryIterator($filePath . "src")) as $file){
-            $path = ltrim(str_replace(["\\", $filePath], ["/", ""], $file), "/");
-            if($path{0} === "." or strpos($path, "/.") !== false or substr($path, 0, 4) !== "src/"){
-                continue;
-            }
-            $phar->addFile($file, $path);
-            $sender->sendMessage("[GenisysPro] Adding $path");
-        }
-        foreach($phar as $file => $finfo){
-            /** @var \PharFileInfo $finfo */
-            if($finfo->getSize() > (1024 * 512)){
-                $finfo->compress(\Phar::GZ);
-            }
-        }
-        $phar->stopBuffering();
+		$filePath = substr(\pocketmine\PATH, 0, 7) === "phar://" ? \pocketmine\PATH : realpath(\pocketmine\PATH) . "/";
+		$filePath = rtrim(str_replace("\\", "/", $filePath), "/") . "/";
+		foreach(new \RecursiveIteratorIterator(new \RecursiveDirectoryIterator($filePath . "src")) as $file){
+			$path = ltrim(str_replace(["\\", $filePath], ["/", ""], $file), "/");
+			if($path{0} === "." or strpos($path, "/.") !== false or substr($path, 0, 4) !== "src/"){
+				continue;
+			}
+			$phar->addFile($file, $path);
+			//$sender->sendMessage("[Turanic] Adding $path");
+		}
+		foreach($phar as $file => $finfo){
+			/** @var \PharFileInfo $finfo */
+			if($finfo->getSize() > (1024 * 512)){
+				$finfo->compress(\Phar::GZ);
+			}
+		}
+		$phar->stopBuffering();
+		$sender->sendMessage($server->getName() . " " . $server->getPocketMineVersion() . " Phar file has been created on " . $pharPath);
 
-        $license = "
-  _____            _               _____           
- / ____|          (_)             |  __ \          
-| |  __  ___ _ __  _ ___ _   _ ___| |__) | __ ___  
-| | |_ |/ _ \ '_ \| / __| | | / __|  ___/ '__/ _ \ 
-| |__| |  __/ | | | \__ \ |_| \__ \ |   | | | (_) |
- \_____|\___|_| |_|_|___/\__, |___/_|   |_|  \___/ 
-                         __/ |                    
-                        |___/         
- ";
-        $sender->sendMessage($license);
-        $sender->sendMessage($server->getName() . " " . $server->getPocketMineVersion() . " Phar file has been created on " . $pharPath);
-
-        return true;
-    }
+		return true;
+	}
 }
